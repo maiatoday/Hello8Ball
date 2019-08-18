3d2b0e46a308b2d700e049203d952679f2909229
maia grotepass <maiatoday@gmail.com>
2019-08-18 19:38:27 +0200

    Add detekt

--- app/src/main/java/net/maiatoday/hello8ball/MyRepository.kt
+++ app/src/main/java/net/maiatoday/hello8ball/MyRepository.kt
@@ -1,13 +1,14 @@
 package net.maiatoday.hello8ball
 
 object MyRepository {
+    const val MAX_ANSWERS = 20
     fun setAnswers(stringArray: Array<String>) {
         if (answers.isEmpty()) {
             answers.addAll(stringArray)
         }
     }
 
-    private val answers: MutableList<String> = ArrayList(20)
+    private val answers: MutableList<String> = ArrayList(MAX_ANSWERS)
 
     fun getAnswer(question:String):String {
 
