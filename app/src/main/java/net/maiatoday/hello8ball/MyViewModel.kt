331684bd15609497155dfde4ce604be8d64b3a9b
maia grotepass <maiatoday@gmail.com>
2019-08-18 18:12:13 +0200

    Initial commit

---
+++ app/src/main/java/net/maiatoday/hello8ball/MyViewModel.kt
@@ -0,0 +1,19 @@
+package net.maiatoday.hello8ball
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    private val answer: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    fun getAnswer(): LiveData<String> {
+        return answer
+    }
+
+    fun fetchAnswer() {
+        answer.value = "Better not tell you now."
+    }
+}
