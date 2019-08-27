3711d48759ba69205a123b1d7197e6eadcf431a2
maia grotepass <maiatoday@gmail.com>
2019-08-27 21:04:22 +0200

    clean is prime

--- app/src/main/java/net/maiatoday/hello8ball/util/NumberHelper.kt
+++ app/src/main/java/net/maiatoday/hello8ball/util/NumberHelper.kt
@@ -1,13 +1,16 @@
 package net.maiatoday.hello8ball.util
 
-fun isPrime(n: Int): Boolean {
+fun isPrime(n: Int): Boolean =
     if (n <= 1) {
-        return false
+        false
     } else if (n <= 3) {
-        return true
+        true
     } else if ((n % 2 == 0) || (n % 3 == 0)) {
-        return false
-    }
+        false
+    } else isPrimeLoop(n)
+
+
+fun isPrimeLoop(n: Int): Boolean {
     for (i in 5..n / 2 step 6) {
         if (n % i == 0 || n % (i + 2) == 0) {
             return false
