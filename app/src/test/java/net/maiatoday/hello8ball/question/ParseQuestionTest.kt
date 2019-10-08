cd59debb0bae641d7ac76b1423f4f1f778a27a31
maia grotepass <maiatoday@gmail.com>
2019-10-08 22:47:58 +0200

    More parameterized tests

--- app/src/test/java/net/maiatoday/hello8ball/question/ParseQuestionTest.kt
+++ app/src/test/java/net/maiatoday/hello8ball/question/ParseQuestionTest.kt
@@ -1,34 +1,32 @@
 package net.maiatoday.hello8ball.question
 
 import com.google.common.truth.Truth.assertWithMessage
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
-@RunWith(Parameterized::class)
-class ParseQuestionTest(val question: String, val questionType: QuestionType) {
+class ParseQuestionTest() {
     companion object {
         @JvmStatic
-        @Parameterized.Parameters
-        fun data() = listOf(
-            arrayOf("password", QuestionType.PASSWORD),
-            arrayOf("Password", QuestionType.PASSWORD),
-            arrayOf("word", QuestionType.SYNONYM),
-            arrayOf("supercalifragilistixexpialidocious", QuestionType.SYNONYM),
-            arrayOf("a", QuestionType.SYNONYM),
-            arrayOf("how is life with dogs 🐶?", QuestionType.OTHER),
-            arrayOf("Is the universe expanding 🌟?", QuestionType.OTHER),
-            arrayOf("771", QuestionType.PRIME),
-            arrayOf("life the universe and everything", QuestionType.BASIC),
-            arrayOf("", QuestionType.OTHER)
+        fun testData():List<Arguments> = listOf(
+            Arguments.of("password", QuestionType.PASSWORD),
+            Arguments.of("Password", QuestionType.PASSWORD),
+            Arguments.of("word", QuestionType.SYNONYM),
+            Arguments.of("supercalifragilistixexpialidocious", QuestionType.SYNONYM),
+            Arguments.of("a", QuestionType.SYNONYM),
+            Arguments.of("how is life with dogs 🐶?", QuestionType.OTHER),
+            Arguments.of("Is the universe expanding 🌟?", QuestionType.OTHER),
+            Arguments.of("771", QuestionType.PRIME),
+            Arguments.of("life the universe and everything", QuestionType.BASIC),
+            Arguments.of("", QuestionType.OTHER)
         )
     }
 
-    @Test
-    fun `parse question should detect question type`() {
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `parse question should detect question type`(question:String, questionType:QuestionType) {
         assertWithMessage("Expect $question to parse to ${questionType}")
             .that(parseQuestion(question))
             .isEqualTo(questionType)
     }
-
 }
