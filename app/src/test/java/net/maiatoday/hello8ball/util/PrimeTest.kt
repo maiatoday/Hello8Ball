3c92a0eba76dfc83daf874b249bdf150da87f0ff
maia grotepass <maiatoday@gmail.com>
2019-10-08 22:36:27 +0200

    Add parameterized primeness tests

--- app/src/test/java/net/maiatoday/hello8ball/util/PrimeTest.kt
+++ app/src/test/java/net/maiatoday/hello8ball/util/PrimeTest.kt
@@ -1,36 +1,24 @@
 package net.maiatoday.hello8ball.util
 
 import com.google.common.truth.Truth
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-@RunWith(Parameterized::class)
-class PrimeTest(val number: Int, val prime: Boolean) {
+class PrimeTest {
 
-    companion object {
-        @JvmStatic
-        @Parameterized.Parameters
-        fun data() = listOf(
-            arrayOf(0, false),
-            arrayOf(1, false),
-            arrayOf(2, true),
-            arrayOf(3, true),
-            arrayOf(4, false),
-            arrayOf(6, false),
-            arrayOf(7, true),
-            arrayOf(9, false),
-            arrayOf(49, false),
-            arrayOf(773, true),
-            arrayOf(772, false)
-        )
+    @ParameterizedTest(name = "{0} is prime")
+    @ValueSource(ints = [2,3,7,773])
+    fun `test for primeness`(value: Int) {
+        Truth.assertWithMessage("Expect $value to be prime")
+            .that(isPrime(value))
+            .isTrue()
     }
 
-    @Test
-    fun `test for primeness`() {
-        Truth.assertWithMessage("Expect $number to be prime=${prime}")
-            .that(isPrime(number))
-            .isEqualTo(prime)
+    @ParameterizedTest(name = "{0} is not prime")
+    @ValueSource(ints = [0,1,4,6,9,49,772])
+    fun `test for not - primeness`(value: Int) {
+        Truth.assertWithMessage("Expect $value to not be prime")
+            .that(isPrime(value))
+            .isFalse()
     }
-
 }
