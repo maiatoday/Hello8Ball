b4d7bd9d80521fa4f3bb96fb74fe0b15c316e947
maia grotepass <maiatoday@gmail.com>
2020-06-28 17:09:20 +0200

    Basic Hilt injection

---
+++ app/src/main/java/net/maiatoday/hello8ball/di/AppModule.kt
@@ -0,0 +1,64 @@
+package net.maiatoday.hello8ball.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import net.maiatoday.hello8ball.question.*
+import net.maiatoday.hello8ball.util.DispatcherProvider
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+    @Qualifier
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class EightBallAnswers
+
+    @Qualifier
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class PasswordAnswers
+
+    @Qualifier
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class SynonymAnswers
+
+    @Singleton
+    @EightBallAnswers
+    @Provides
+    fun provideQuestion8Ball(): QuestionInterface {
+        return QuestionEightBall
+    }
+
+    @Singleton
+    @PasswordAnswers
+    @Provides
+    fun provideQuestionPassword(): QuestionInterface {
+        return QuestionPassword()
+    }
+
+    @Singleton
+    @SynonymAnswers
+    @Provides
+    fun provideQuestionSynonym(): QuestionInterface {
+        return QuestionSynonym()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDispatcher(): DispatcherProvider {
+        return DispatcherProvider()
+    }
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(
+        @EightBallAnswers eightBall: QuestionInterface,
+        @PasswordAnswers password: QuestionInterface,
+        @SynonymAnswers synonym: QuestionInterface,
+        provider: DispatcherProvider
+    ): QuestionRepository {
+        return QuestionRepository(eightBall, password, synonym, provider)
+    }
+}
